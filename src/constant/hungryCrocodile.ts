55c668b2599e984984ccc3274e161a1f966d8274
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-05-13 14:12:32 +0800

    fix: add style to ballon game party

--- src/constant/hungryCrocodile.ts
+++ src/constant/hungryCrocodile.ts
@@ -1,6 +1,6 @@
 export const LINE_POSITIONING = [8, 20, 33, 45, 57, 69, 81];
 export const SPACE_POSITIONING = [2, 14, 26, 39, 51, 63, 76, 89];
-export const GAME_TIME = 90;
+export const GAME_TIME = 75;
 export const SCORE_BOUNDARY = 10;
 
 export default { GAME_TIME, LINE_POSITIONING, SCORE_BOUNDARY, SPACE_POSITIONING };
