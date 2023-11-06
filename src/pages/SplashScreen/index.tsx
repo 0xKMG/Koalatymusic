7198962d9e04a6d49632f6c58eeab55b9e118e71
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-11-06 12:03:09 +0800

    merge changes from bitbucket

--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -1,19 +1,25 @@
 import { useContext, useEffect } from 'react';
 import { Context } from 'context/store';
 import { Logo } from 'assets/svgs';
+import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 const SplashScreen = (): JSX.Element => {
+  const navigate = useNavigate();
   const { setStart } = useContext(Context);
 
   useEffect(() => {
     setStart(true);
   }, []);
 
+  const gameSelection = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <div className={styles.splashScreen}>
       <div className={styles.logo}>
-        <button type="button">
+        <button type="button" onClick={() => gameSelection('/menu')}>
           <Logo width="100%" />
         </button>
       </div>
